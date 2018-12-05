6e384d57727c6ebe943cff2829676e783978e261
Vasavi <vasavivempati2@gmail.com>
2018-12-05 15:46:03 -0800

    adding tests

--- src/test/java/zipCode/RangeTest.java
+++ src/test/java/zipCode/RangeTest.java
@@ -7,66 +7,40 @@ import java.util.ArrayList;
 
 
 class RangeTest  {
- @Test
-  void testOnlyAddZipCodes(){
-    ArrayList<Range> sampleInput = new ArrayList<Range> ();
-    Range sample = new Range();
-     Range sample1 = new Range();
-     Range sample2 = new Range();
+    ArrayList<Range> sampleInput(){
+        ArrayList<Range> sampleInput = new ArrayList<Range> ();
+        Range sample = new Range();
+        Range sample1 = new Range();
+        Range sample2 = new Range();
 
-     sample.setFloor(94133);
-    sample.setCeiling(94133);
-    sampleInput.add(0,sample);
-    sample1.setFloor(94200);
-    sample1.setCeiling(94299);
-    sampleInput.add(1,sample1);
-    sample2.setFloor(94226);
-    sample2.setCeiling(94399);
-    sampleInput.add(2,sample2);
-    for(int i=0; i<3;i++){
-        System.out.print(sampleInput.get(i).getCeiling());
-        System.out.print("   ");
-        System.out.print(sampleInput.get(i).getFloor());
-        System.out.println("h");
-
-
+        sample.setFloor(94133);
+        sample.setCeiling(94133);
+        sampleInput.add(0,sample);
+        sample1.setFloor(94200);
+        sample1.setCeiling(94299);
+        sampleInput.add(1,sample1);
+        sample2.setFloor(94226);
+        sample2.setCeiling(94399);
+        sampleInput.add(2,sample2);
+        return sampleInput;
     }
+    @Test
+  void testOnlyAddZipCodes(){
     ZipCodeParser addZips = new ZipCodeParser();
-    addZips.add_element(sampleInput);
+    addZips.add_element(sampleInput());
   }
   @Test
     void testDeleteDuplicateElements()
   {
       ArrayList<Range> sortedRanges = new ArrayList<Range>();
-      ArrayList<Range> sampleInput = new ArrayList<Range> ();
-      Range sample = new Range();
-      Range sample1 = new Range();
-      Range sample2 = new Range();
-
-      sample.setFloor(94133);
-      sample.setCeiling(94133);
-      sampleInput.add(0,sample);
-      sample1.setFloor(94200);
-      sample1.setCeiling(94299);
-      sampleInput.add(1,sample1);
-      sample2.setFloor(94226);
-      sample2.setCeiling(94399);
-      sampleInput.add(2,sample2);
-      for(int i=0; i<3;i++){
-          System.out.print(sampleInput.get(i).getCeiling());
-          System.out.print("   ");
-          System.out.print(sampleInput.get(i).getFloor());
-          System.out.println("h");
-
-
-      }
       ZipCodeParser addZips = new ZipCodeParser();
-      sortedRanges = addZips.add_element(sampleInput);
+      sortedRanges = addZips.add_element(sampleInput());
       Boolean duplicate = false;
       for (Range r:sortedRanges) {
           addZips.eliminateDuplicates(sortedRanges,r);
       }
   }
+
 
  }
 
